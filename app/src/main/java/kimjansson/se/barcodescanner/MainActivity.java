d86e2d518e3e9589d96e98c330aee26ae3118e07
Kim Jansson <kimlennart77@gmail.com>
2015-03-27 13:29:10 +0100

    Still testing
    Getting used to GitHub for windows.

--- app/src/main/java/kimjansson/se/barcodescanner/MainActivity.java
+++ app/src/main/java/kimjansson/se/barcodescanner/MainActivity.java
@@ -3,7 +3,6 @@ package kimjansson.se.barcodescanner;
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,13 +13,13 @@ import com.google.zxing.integration.android.IntentResult;
 public class MainActivity extends Activity {
 
     private Button scanButton;
-    private TextView scanOutput;
+    private TextView scanOutputWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        scanOutput = (TextView) findViewById(R.id.scan_output);
+        scanOutputWindow = (TextView) findViewById(R.id.scan_output);
         // Scanner
         scanButton = (Button) findViewById(R.id.scan_button);
         scanButton.setOnClickListener(new View.OnClickListener() {
@@ -37,8 +36,7 @@ public class MainActivity extends Activity {
         IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanResult != null) {
             String result = scanResult.getContents();
-            scanOutput.setText("");
-            scanOutput.setText(result);
+            scanOutputWindow.setText(result);
         }
         // Added as test
         // else continue with any other code you need in the method
