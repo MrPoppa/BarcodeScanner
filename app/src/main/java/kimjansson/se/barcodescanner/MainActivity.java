432b1517fd7651ed9a0e571dca51d79843a6a9a9
Kim Jansson <kimlennart77@gmail.com>
2015-03-27 13:13:17 +0100

    Initial Commit

---
+++ app/src/main/java/kimjansson/se/barcodescanner/MainActivity.java
@@ -0,0 +1,45 @@
+package kimjansson.se.barcodescanner;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends Activity {
+
+    private Button scanButton;
+    private TextView scanOutput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scanOutput = (TextView) findViewById(R.id.scan_output);
+        // Scanner
+        scanButton = (Button) findViewById(R.id.scan_button);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan();
+            }
+        });
+
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+        if (scanResult != null) {
+            String result = scanResult.getContents();
+            scanOutput.setText(result);
+        }
+        // else continue with any other code you need in the method
+
+    }
+}
